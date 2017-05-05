9fed683235ebf90ecfb69434b82ffd53df44c031
James Jucutan <jljucutan@gmail.com>
2017-05-05 10:35:28 +0700

    added some files

---
+++ web/assets/js/DUBAI_New_Starter_Form.js
@@ -0,0 +1,14 @@
+$(function() {
+    
+
+if (typeof jQuery != 'undefined') {  
+    // jQuery is loaded => print the version
+    alert(jQuery.fn.jquery);
+}
+
+
+    $('[data-radio]').val();
+    // $('[data-radio="true"]').on('input[type="checkbox"]', 'click', function() {
+        // console.log("checked");
+    // });
+});
