d18c567752790a1e9f0227815aa62c7fab80e9c7
James Jucutan <jljucutan@gmail.com>
2017-05-26 15:34:21 +0700

    form formats

--- web/assets/js/401k_Beneficiary_Form.js
+++ web/assets/js/401k_Beneficiary_Form.js
@@ -1,3 +1,6 @@
 $(function() {
     $('[data-radio-group]').makeRadio();
+    $('#first_name, #last_name').on('change paste keyup', function() {
+        $('#full_name').val($('#first_name').val() + ' ' + $('#last_name').val());
+    });
 });
