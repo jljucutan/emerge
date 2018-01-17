4c1711a041340653017fe2875147ebff6dba88e9
jljucutan <jljucutan@gmail.com>
2018-01-17 16:21:51 +0800

    temp templates

--- web/assets/js/various.js
+++ web/assets/js/various.js
@@ -4,3 +4,19 @@ $(document).ready(function() {
     .find('.calendar_button')
     .prop('disabled', true);
 });
+
+
+function hasLengthLimit(sValue, sName, sField) {
+  var isValid = true;
+  if('<$client.env.serversidevalidation>' == '1'){return isValid;}
+  var strVal = sValue.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+  if (strVal.length == 0) {
+    eFormRequiredField(sValue, sName, sField);
+  }
+  var maxlength = document.getElementById(sField).getAttribute('maxlength');
+  if (strVal.length != maxlength) {
+    AddError(sField, 'Error in validation, it should have ' + maxlength + ' digits in', '');
+    isValid = false;
+  }
+  return isValid;
+}
